0b14cef88df5eff6883c24f70bad2e553ac5f61c
Damini Purohit <damini.purohit@continuum.net>
2021-02-06 14:08:26 +0530

    Updates

---
+++ envconfig.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-unused-vars
+function getBaseURL () {
+    // NOTE : local api to test should be added here.
+    return 'https://tw-frontenders.firebaseio.com';
+}
+
+// for different envs, DT, QA, PROD , we can maintain separate files in app or outside and load it during deployment
